620b2ef7e2d572304651fdffd68c3b5300d1ab9b
Sigrid Ho <xinruihe@Xinruis-MBP.wv.cc.cmu.edu>
2017-02-13 18:42:56 -0500

    feature classification test

--- classification.py
+++ classification.py
@@ -1,6 +1,8 @@
 import pickle
 import math
-from statistics import *
+import sys
+from index import *
+from feature import *
 from collections import Counter
 
 
@@ -8,32 +10,34 @@ from collections import Counter
 if __name__ == '__main__':
 	filename = sys.argv[1]
 	f = open(filename, 'r')
+	s = Index.load('index')
 	feature = Feature()
 	lines = f.readlines()
 	threshold = [20, 0.001, 0.1]
+	features = [0,0,0]
 	numFeatures = 3
+	miss = 0
 	for line in lines:
 		line = line.split(',')
-        title = line[0]
-        title = title.split(' ')
-        bodyId = line[1]
-        related = line[2]
-		#Todo: get body from bodyId
-		body = getBody(bodyId)
-		features = []
+		title = line[0]
+		title = title.split(' ')
+		bodyId = line[1]
+		related = line[2]
+		body = s.getBody(bodyId)
 		features[0] = feature.BM25(title, bodyId, body)
 		features[1] = feature.Indri(title, bodyId, body)
 		features[2] = feature.cosSim(title, bodyId, body)
 		count = 0
 		for i in range(numFeatures):
 			if features[i] >= threshold[i]:
-				count++
-
+				count += 1
 		if (count > (numFeatures >> 1)):
 			print "Prediction: Related.\n"
 		else:
 			print "Prediction: Unrelated.\n"
-
+			if not related.startswith('un'):
+				miss += 1
 		print "Standard realtion: " + related + "\n"
 		print "************"
+	print miss
 
